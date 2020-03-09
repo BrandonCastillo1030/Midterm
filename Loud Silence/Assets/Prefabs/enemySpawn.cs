ed9bd9307ddd438c92cc7763cca4e05537770d79
Q Blake <qblake139@gmail.com>
2020-03-09 13:30:44 -0400

    chris finally did something, with enemy spawn and movement

---
+++ Loud Silence/Assets/Prefabs/enemySpawn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    void Start()
+    {
+        
+        for (int i = -5; i < 50; i++)
+        {
+            Instantiate(enemy, new Vector3(UnityEngine.Random.Range(-40,10) ,3, i), Quaternion.identity);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Loud Silence/Assets/Prefabs/rotationEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotationSpeed;
+    public float smoothSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate((Vector3.up * rotationSpeed) * (Time.deltaTime * smoothSpeed), Space.Self);
+    }
+}
