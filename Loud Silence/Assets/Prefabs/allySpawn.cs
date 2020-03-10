e038619878a0fbcb79e340ee5a76205921c029ad
Jonathan Clement <blowdacon@gmail.com>
2020-03-10 14:22:55 -0400

    Added Ally's
    added ally spawner

---
+++ Loud Silence/Assets/Prefabs/allySpawn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allySpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ally;
+    void Start()
+    {
+
+        for (int i = -5; i < 20; i++)
+        {
+            Instantiate(ally, new Vector3(UnityEngine.Random.Range(-40, 10), 3, i), Quaternion.identity);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
