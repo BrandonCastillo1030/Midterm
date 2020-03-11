9bf1be29d96e7e3aa98c5cafc2a8f8adfed9b494
Jonathan Clement <blowdacon@gmail.com>
2020-03-10 21:29:49 -0400

    Added Controls Menu

---
+++ Loud Silence/Assets/MainMenu/Controls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour
+{
+    public void ControlMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+    
+}
